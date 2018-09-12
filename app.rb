d01827739e979c7ead3814076206bbb82ba48b9b
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-12 16:50:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    text_from_user = params[:user_text]
+    @pig_text = 
+    erb :results
+  end
+
 end
