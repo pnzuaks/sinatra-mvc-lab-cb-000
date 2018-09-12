838c29f277cec607f4ac0e1f9efe8e395915bc4e
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-12 17:09:51 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,47 +1,53 @@
 class PigLatinizer
   attr_accessor :sent
 
-    def piglatinize(sent)
-        sent.gsub('.','').split(' ').collect do |word|
-          translate word
-        end.compact.join(' ')
-      end
+  def piglatinize(sent)
 
-      def translate(word)
-        if word.length > 1
-          if word == 'and' || word == 'an' || word == 'in'
-            word
-          elsif capture = consonant_expression.match(word)
-            capture.post_match.to_s + capture.to_s + 'ay'
-          elsif vowel?(first_letter=word[0].downcase)
-            word + 'ay'
-          elsif vowel?(last_letter=word[-1].downcase)
-            move_last_letter(word) + 'ay'
-          end
-        else
-          word
+    sent = sent.downcase
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    words = sent.split(' ')
+    result = []
+
+words.each_with_index do |word, i|
+    translation = ''
+    qu = false
+    if vowels.include? word[0]
+        translation = word + 'ay'
+        result.push(translation)
+    else
+        word = word.split('')
+        count = 0
+        word.each_with_index do |char, index|
+            if vowels.include? char
+                # handle words that start with 'qu'
+                if char == 'u' and translation[-1] == 'q'
+                    qu = true
+                    translation = words[i][count + 1..words[i].length] + translation + 'uay'
+                    result.push(translation)
+                    next
+                end
+                break
+            else
+                # handle words with 'qu' in middle
+                if char == 'q' and word[i+1] == 'u'
+                    qu = true
+                    translation = words[i][count + 2..words[i].length] + 'quay'
+                    result.push(translation)
+                    next
+                else
+                    translation += char
+                end
+                count += 1
+            end
         end
-      end
+        # translation of consonant words without qu
+        if not qu
+            translation = words[i][count..words[i].length] + translation + 'ay'
+            result.push(translation)
+        end
+    end
 
-      # Move last letter to beginning of word
-      def move_last_letter(word)
-        word[-1] +  word[0..-2]
-      end
-
-      private
-      def consonant_expression
-        # at the beginning of a String
-        # capture anything not a vowel (consonants)
-        # capture 1, 2 or 3 occurences
-        # ignore case and whitespace
-        /^ [^aeiou] {1,3}/ix
-      end
-
-      def vowel?(letter)
-        vowels.include?(letter)
-      end
-
-      def vowels
-        %w[a e i o u]
-      end
+end
+result.join(' ')
+end
 end
